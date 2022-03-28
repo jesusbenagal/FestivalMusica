93c1566b2ba991d0f275f799aed5000ccb6b00c8
jesusbenagal <jesusguerreropaniagua6@gmail.com>
2022-03-28 19:53:30 +0200

    aligerando imagenes

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
-const { src, dest, watch } = require('gulp');
+const { src, dest, watch, parallel } = require('gulp');
+
+//CSS
 const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
+
+//Imagenes
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
 
 function css( done ) {    
     src('src/scss/**/*.scss') //Identificar el archivo SASS
@@ -10,10 +17,37 @@ function css( done ) {
     done();
 }
 
+function imagenes(done) {
+    
+    const opciones = {
+        optimizationLevel: 3
+    }
+
+    src('src/img/**/*.{png,jpg}')
+        .pipe( cache( imagemin(opciones) ) )
+        .pipe( dest('build/img' ) ) 
+    done();
+}
+
+function versionWebp( done ) {
+
+    const opciones = {
+        quality: 50
+    };
+
+    src('src/img/**/*.{png,jpg}')
+        .pipe( webp(opciones) )
+        .pipe( dest('build/img') )
+
+    done();
+}
+
 function dev ( done ) {
     watch('src/scss/**/*.scss', css);
     done();
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.imagenes = imagenes
+exports.versionWebp = versionWebp;
+exports.dev = parallel ( imagenes, versionWebp, dev );
