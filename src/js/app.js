dccd568a1204ac86fed87087e309d239b871255c
jesusbenagal <jesusguerreropaniagua6@gmail.com>
2022-04-06 20:22:52 +0200

    Acabando galeria

--- src/js/app.js
+++ src/js/app.js
@@ -16,8 +16,51 @@ function crearGaleria() {
             <source srcset="build/img/thumb/${i}.webp" type="image/webp">
             <img loading="lazy" width="200" height="300"src="build/img/thumb/${i}.jpg" alt="imagen_galeria">
         `
+        imagen.onclick = function () {
+            mostrarImagen(i);
+        }
+
         galeria.appendChild(imagen);
         
     }
 
+}
+
+function mostrarImagen(id) {
+    const imagen = document.createElement('picture');
+    imagen.innerHTML = `
+        <source srcset="build/img/grande/${id}.avif" type="image/avif">
+        <source srcset="build/img/grande/${id}.webp" type="image/webp">
+        <img loading="lazy" width="200" height="300"src="build/img/grande/${id}.jpg" alt="imagen_galeria">
+    `
+
+    //Crear el overlay
+    const overlay = document.createElement('DIV');
+    overlay.appendChild(imagen);
+    overlay.classList.add('overlay');
+    overlay.onclick = function () {
+
+        const body = document.querySelector('body');
+        body.classList.remove('fijar-body')
+        overlay.remove();
+
+    }
+
+    //Boton cerrar modal
+    const cerrarModal = document.createElement('P');
+    cerrarModal.textContent = 'X';
+    cerrarModal.classList.add('btn-cerrar');
+    cerrarModal.onclick = function () {
+
+        const body = document.querySelector('body');
+        body.classList.remove('fijar-body')
+        overlay.remove();
+        
+    }
+    overlay.appendChild(cerrarModal);
+
+    //Añadir al HTML
+    const body = document.querySelector('body');
+    body.appendChild(overlay)
+    body.classList.add('fijar-body')
 }
